f1d8ef3613ce15917a06cbbdc9a8220a89923a81
pokkew <brunogabrielpk@gmail.com>
2019-10-28 21:44:22 -0400

    Parte 2 aparentemente funcional

--- Plan_aula/models.py
+++ Plan_aula/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from docentes.models import Docente
+
 
 # Create your models here.
+
+
 
 class PlanA(models.Model):
     uc = models.CharField(max_length=100)
     evento = models.CharField(max_length=20)
     ch = models.IntegerField()
     obj = models.TextField(max_length=1000)
-    docente = models.CharField(max_length=150)
+    docente = models.ForeignKey(Docente,on_delete=models.CASCADE)
+
     
     def __str__(self):
         return self.uc
-    
+
--- itemP/views.py
+++ itemP/views.py
@@ -17,30 +17,32 @@ def itemP_list(request,pk, template_name='itemP/itemp_list.html'):
     data['iden'] = pk
     return render(request,template_name,data)
 
+
 def itemP_create(request,pk, template_name='itemP/itemp_form.html'):
-    plano = PlanA.objects.get(id = pk)
+    plano = PlanA.objects.get(id=pk)
     form = Item_planForm(request.POST or None)
     form.fields['plana'].initial = plano.id
     form.fields['plana'].disabled = True
     if form.is_valid():
         form.save()
-        return redirect('Plan_aula:itemP:itemp_list',pk=plano.id)
-    return render(request, template_name, {'form':form})
+        return redirect('Plan_aula:itemP:itemp_list', pk=plano.id)
+    return render(request, template_name, {'form': form, 'plano': plano})
+
 
 def itemP_update(request, pkI, template_name='itemP/itemp_form.html'):
     itemplan = get_object_or_404(Item_plan, pk=pkI)
-    plano = PlanA.objects.get(id = itemplan.plana_id)
+    plano = PlanA.objects.get(id=itemplan.plana_id)
     form = Item_planForm(request.POST or None, instance=itemplan)
     form.fields['plana'].disabled = True
     if form.is_valid():
         form.save()
-        return redirect('Plan_aula:itemP:itemp_list',pk=plano.id)
-    return render(request, template_name, {'form':form})
+        return redirect('Plan_aula:itemP:itemp_list', pk=plano.id)
+    return render(request, template_name, {'form': form, 'plano': plano})
 
 def itemP_delete(request, pkI, template_name='itemP/itemp_confirm_delete.html'):
     itemplan = get_object_or_404(Item_plan, pk=pkI)
     plano = PlanA.objects.get(id = itemplan.plana_id)
     if request.method=='POST':
         itemplan.delete()
-        return redirect('Plan_aula:itemP:itemp_list',pk=plano.id)
+        return redirect('Plan_aula:itemP:itemp_list', pk=plano.id)
     return render(request, template_name, {'object':itemplan})
