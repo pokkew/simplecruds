42e8e63754cd2d385b1d8ab0efa1766a161a3a4b
pokkew <brunogabrielpk@gmail.com>
2019-10-29 16:41:52 -0400

    Login - iniciado a fase 3

--- apps/settings.py
+++ apps/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'apps.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,3 +136,6 @@ STATICFILES_DIRS = [
 
 ]
 LOGIN_URL = '/admin/login/'
+
+# my_project/settings.py
+LOGIN_REDIRECT_URL = '/'
--- apps/urls.py
+++ apps/urls.py
@@ -5,6 +5,7 @@ from theme import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('books_cbv/', include('books_cbv.urls', namespace='books_cbv')),
     path('books_fbv/', include('books_fbv.urls', namespace='books_fbv')),
     path('books_fbv_user/', include('books_fbv_user.urls', namespace='books_fbv_user')),
