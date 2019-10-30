6b8071b91d32f45c7b3798b8a6ca4e2873a25e32
pokkew <brunogabrielpk@gmail.com>
2019-10-30 08:15:10 -0400

    Adicionado user.id aos planos de aula

--- Plan_aula/models.py
+++ Plan_aula/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from docentes.models import Docente
-
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
 
 
 class PlanA(models.Model):
@@ -11,7 +15,8 @@ class PlanA(models.Model):
     evento = models.CharField(max_length=20)
     ch = models.IntegerField()
     obj = models.TextField(max_length=1000)
-    docente = models.ForeignKey(Docente,on_delete=models.CASCADE)
+    docente = models.ForeignKey(Docente, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(get_sentinel_user), default=1)
 
     
     def __str__(self):
