2e0b87c4b042e4583313d2fcdaf0cd9e9dba56e9
pokkew <brunogabrielpk@gmail.com>
2019-11-07 15:56:29 -0400

    asterisco

--- apps/settings.py
+++ apps/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 #ALLOWED_HOSTS = ['https://simplecruds.herokuapp.com/']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
