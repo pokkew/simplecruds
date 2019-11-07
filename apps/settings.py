4d1ce61084c62912abcbd7b55fe87bbcc90825d6
pokkew <brunogabrielpk@gmail.com>
2019-11-06 22:59:31 -0400

    static blabla

--- apps/settings.py
+++ apps/settings.py
@@ -142,15 +142,18 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 
 ]
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 LOGIN_URL = '/admin/login/'
 
 # my_project/settings.py
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
