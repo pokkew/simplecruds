339d06ae6c76f3625ac7d432353cd1f7c7faeba0
pokkew <brunogabrielpk@gmail.com>
2019-11-06 22:51:34 -0400

    ajustando statics

--- apps/settings.py
+++ apps/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'docentes',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = {
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,7 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+}
 
 ROOT_URLCONF = 'apps.urls'
 
@@ -134,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
 
@@ -146,6 +148,9 @@ LOGIN_URL = '/admin/login/'
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 
-# Static
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
