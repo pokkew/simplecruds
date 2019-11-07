81115a3426ec81fdb31e97ca916fb645d3c2c055
pokkew <brunogabrielpk@gmail.com>
2019-11-07 14:07:25 -0400

    midlewares do inferno

--- apps/settings.py
+++ apps/settings.py
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = {
     "django.middleware.security.SecurityMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
